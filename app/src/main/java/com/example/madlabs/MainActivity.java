7d2b3671401e7b045d1b6bce1ab5ea804a2bb512
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2021-08-21 17:05:12 +0530

    First Commit

---
+++ app/src/main/java/com/example/madlabs/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.madlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    Button btnStop;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+
+        btnStart = (Button)findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MyServices.class));
+            }
+        });
+
+        btnStop = (Button)findViewById(R.id.button2);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MyServices.class));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/madlabs/Service.java
@@ -0,0 +1,6 @@
+package com.example.madlabs;
+
+public class Service {
+
+
+}
